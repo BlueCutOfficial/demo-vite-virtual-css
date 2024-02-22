dc88e2b2d5de7128309012d09795a01034a846b1
Marine Dunstetter <bluecut.official@gmail.com>
2024-02-22 14:28:58 +0100

    seems to work when the ?direct is in the resolved id rather than the id to resolve

--- plugins/vite-plugin-virtual-file.js
+++ plugins/vite-plugin-virtual-file.js
@@ -1,33 +1,20 @@
 export default function vitePluginVirtualFile() {
-  const virtualHelloId = '@my-vite-app/hello'
-  const resolvedVirtualHelloId = 'virtual:hello'
 
-  const virtualStyleId = '@my-vite-app/style.css'
-  const resolvedVirtualStyleId = 'virtual:style.css'
+  const virtualStyleId = '@my-vite-app/vstyle.css'
+  const resolvedVirtualStyleId = '@my-vite-app/vstyle-resolved.css?direct'
 
   return {
     name: 'vite-plugin-virtual-file',
     resolveId(id) {
-      if (id === virtualHelloId) {
-        return resolvedVirtualHelloId
-      }
-      if (id === virtualStyleId) {
-        return resolvedVirtualStyleId
+      if (id.includes(virtualStyleId)) {
+        return resolvedVirtualStyleId;
       }
     },
     load(id) {
-      if (id === resolvedVirtualHelloId) {
-        return `export const msg = "hello from virtual module"`
-      }
       if (id === resolvedVirtualStyleId) {
         return `h2 { color: blue; padding: 40px; }`
       }
       return null
-    },
-    transform(code, id) {
-      if (id === resolvedVirtualStyleId) {
-        return code;
-      }
     },
   }
 }
